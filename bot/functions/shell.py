8b3fa4f1347004340146de4842b1d599a7521ef6
bowchaw <105728993+bowchaw@users.noreply.github.com>
2022-05-25 16:21:51 +0530

    Update shell.py

--- bot/functions/shell.py
+++ bot/functions/shell.py
@@ -42,6 +42,6 @@ def shell(update, context):
 
 
 SHELL_HANDLER = CommandHandler(
-    BotCommands.ShellCommand sh, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True
+    BotCommands.ShellCommand, shell, sh, r, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True
 )
 dispatcher.add_handler(SHELL_HANDLER)
